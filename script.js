179e9a727766fecd28b287d1b0f601ab55cb2d23
AffanShaikh100 <133389464+AffanShaikh100@users.noreply.github.com>
2023-09-11 20:04:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 //your JS code here. If required.
-function onDOMLoad() {
-        document.body.textContent = "DOM load success";
-      }
-document.addEventListener("DOMContentLoaded", onDOMLoad);
+// function onDOMLoad() {
+//         document.body.textContent = "DOM load success";
+//       }
+// document.addEventListener("DOMContentLoaded", onDOMLoad);
+
+document.addEventListener("DOMContentLoaded",(ondom)=>{
+	document.body.textContent="DOM load success";
+})
